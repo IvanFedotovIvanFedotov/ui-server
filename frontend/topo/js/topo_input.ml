6eb71ec63de9b261c0e7ae32fc9d76c19c075c04
monstasat <sashayanin@gmail.com>
2019-02-14 13:50:37 +0300

    moved scaffold styles to components

--- frontend/topo/js/topo_input.ml
+++ frontend/topo/js/topo_input.ml
@@ -11,15 +11,22 @@ let label_markup text =
   Tyxml_js.Html.(
     span ~a:[a_class ["topology__input__label"]] [txt text])
 
+let make_icon ?classes path =
+  let open Components.Icon.Markup.SVG in
+  let path = create_path path () in
+  let icon = create ?classes [path] () in
+  icon
+
 let markup (input : Common.Topology.topo_input) =
   (* FIXME migrate to svg icon *)
   let name = Common.Topology.get_input_name input in
   Tyxml_js.Html.(
     div ~a:[a_class ["mdc-chip"; "topology__input"]]
       [ div ~a:[a_class ["mdc-chip__text"]] [txt name]
-      ; i ~a:[a_class ["material-icons mdc-chip__icon mdc-chip__icon--trailing"]]
-          [txt "arrow_forward"]])
-
+      ; make_icon
+          ~classes:["mdc-chip__icon mdc-chip__icon--trailing"]
+          Components.Icon.SVG.Path.arrow_right
+      ])
 
 class t ~input () =
   let body = Tyxml_js.To_dom.of_element @@ markup input in
